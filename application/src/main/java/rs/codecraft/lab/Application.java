1f6787e1dde3b4b8c1421fc6866662d7f58f83fc
Djordje Cvetkovic <djolec987@gmail.com>
2022-11-29 00:00:48 +0100

    - Removed all configuration I think is unnecessary - Removed logging

--- application/src/main/java/rs/codecraft/lab/Application.java
+++ application/src/main/java/rs/codecraft/lab/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EntityScan({"rs.codecraft.lab.adapters.database.entity"})
+@EntityScan
 public class Application {
 
 	public static void main(String[] args) {
