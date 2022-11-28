332f9031ea13946022df0c2bc3e1bb6887fa0d24
Djordje Cvetkovic <djolec987@gmail.com>
2022-11-29 00:24:31 +0100

    Altered initial DB data

--- adapters/database/src/main/resources/db/migration/V002__UserData.sql
+++ adapters/database/src/main/resources/db/migration/V002__UserData.sql
@@ -1,3 +1,3 @@
-INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('Djordje', 'Cvetkovic', 'djolec987@gmail.com', 'fixxer');
-INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('Pavle', 'Cvetkovic', 'pavlec018@gmail.com', 'fixxer');
-INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('Aleksandra', 'Cvetkovic', 'alexc987@gmail.com', 'fixxer');
+INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('John', 'Smith', 'js@example.com', 'jsPassword');
+INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('Paul', 'Newman', 'pn@example.com', 'pnPassword');
+INSERT INTO public."user" (first_name, last_name, email, password) VALUES ('Alex', 'Jones', 'aj@example.com', 'ajPassword');
